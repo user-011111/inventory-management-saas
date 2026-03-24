5f322a277d732e7ee4fcbe3cfb155b40980c9a8d
frontend-fayza <fayzabouazza884@gmail.com>
2026-03-25 00:04:50 +0100

    Frontend: AddUser logic and fixed API endpoints

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// Importation de Bootstrap (à installer via npm install bootstrap)
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
